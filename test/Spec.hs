488620a591c73289b3bd658c0d74077be1e6ca94
Chad Laing <chadlaing@inoutbox.com>
2017-08-30 16:27:34 -0600

    Added tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,9 +15,20 @@ main = hspec $ do
   describe "Bio.Sequence.Manipulation.compChar" $ do
     it "complements any nucleotide sequence" $ do
       compChar 'A' `shouldBe` 'T'
+      compChar 'v' `shouldBe` 'B'
+      compChar 'X' `shouldBe` 'X'
 
+  describe "Bio.Sequence.Manipulation.revCompSeqData" $ do
+    it "reverse complements any Bio.Core.SeqData" $ do
+      revCompSeqData (SeqData "AATGCN") `shouldBe` SeqData "NGCATT"
 
+  describe "Bio.Sequence.Manipulation.revQualData" $ do
+    it "reverses any Bio.Core.QualData" $ do
+      revQualData (QualData "RGB%!@%") `shouldBe` QualData "%@!%BGR"
 
+  describe "Bio.Sequence.Manipulation.revComp'" $ do
+    it "adds a reverse complemented Char to the head of a growing Data.ByteString.Lazy.Char8 raw sequence" $ do
+      revComp' "CCGCRAN" 'A' `shouldBe` "TCCGCRAN"
 
 
 --FastQSeq{qSeqData = SeqData "ATCG"
